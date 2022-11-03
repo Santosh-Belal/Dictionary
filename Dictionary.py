6dd5a0b2c514452203569c69deddcf0f5bfd5b90
Santosh Belal <107342749+Santosh-Belal@users.noreply.github.com>
2022-11-03 19:13:49 +0545

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,30 @@
+import pyfiglet
+
+s=pyfiglet.figlet_format("Dictionary")
+
+print(s)
+
+print("_"*65)
+
+print("\n")
+
+Dictionary={
+
+"Name" : "Santosh",
+
+"Class":"Nine",
+
+"Age" : 15,
+
+"Colors":["Red","Blue","Yellow"]
+
+} 
+
+print(Dictionary)
+
+print(len(Dictionary))
+
+print(Dictionary["Name"])
+
+print(Dictionary["Class"])
+
